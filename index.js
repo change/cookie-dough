8b109112f9da9d2a49a958875ed7a06dce2dd18f
Josh Callender <jcallender@change.org>
2014-12-09 20:22:26 -0800

    Fixed a possible explosion of cookies because of where the cookies were defined

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 var cookie = require('cookie'),
-    isServer = !!(typeof window === 'undefined'),
-    parsedCookies;
+    isServer = !!(typeof window === 'undefined');
 
-function get(key) {
-  return parsedCookies[key];
-}
-
-function set(name, value, options) {
-  parsedCookies[name] = value;
+function CookieDough(req) {
+  var parsedCookies;
 
   if (isServer) {
-    var res = options && options.res;
-    if (!res) {
-      throw new Error('Must specify `res` when setting cookie.');
+    if (!req) {
+      throw new Error('Must specify `req` for cookie-dough');
     }
 
-    res.cookie(name, value, options);
-  } else {
-    document.cookie = cookie.serialize(name, value, options);
-  }
-}
-
-
-module.exports = function (req) {
-  if (isServer) {
     parsedCookies = req.cookies;
   } else {
     parsedCookies = cookie.parse(document.cookie);
   }
 
-  return {
-    get: get,
-    set: set,
-    remove: function (key) {
-      set(key, '', { expires: new Date(0) })
+  this.get = function get(name) {
+    return parsedCookies[name];
+  };
+
+  this.set = function set(name, value, options) {
+    parsedCookies[name] = value;
+
+    if (isServer) {
+      var res = options && options.res;
+      if (!res) {
+        throw new Error('Must specify `res` when setting cookie');
+      }
+
+      res.cookie(name, value, options);
+    } else {
+      document.cookie = cookie.serialize(name, value, options);
     }
   };
+
+  this.remove = function remove(name) {
+    this.set(key, '', { expires: new Date(0) });
+    delete parsedCookies[name];
+  };
+
 }
+
+module.exports = CookieDough;
