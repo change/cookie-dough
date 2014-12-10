020dfcca51b48bc1f8fd9bc4ec9d156cace285b9
Josh Callender <jcallender@change.org>
2014-12-09 17:39:34 -0800

    changed the interface to use .get .set and .remove instead of trying to just have one function do it all.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-global.Cookie = require('./cookie-dough')
+var cookie = require('cookie')
+  , isServer = !!(typeof window === 'undefined')
+  , parsedCookies
+
+function get(key) {
+  return parsedCookies[key]
+}
+
+function set(name, value, options) {
+  parsedCookies[name] = value;
+
+  if (isServer) {
+    var res = options && options.res;
+    if (!res) throw new Error('Must specify `res` when setting cookie.');
+    res.cookie(name, value, options);
+  } else {
+    document.cookie = cookie.serialize(name, value, options);
+  }
+}
+
+
+module.exports = function (req) {
+  // should this be so strongly tied to express?
+  if (isServer) {
+    parsedCookies = req.cookies;
+  } else {
+    parsedCookies = cookie.parse(document.cookie);
+  }
+
+  return {
+    get: get,
+    set: set,
+    remove: function (key) {
+      set(key, '', { expires: new Date(0) })
+    }
+  }
+}
+
