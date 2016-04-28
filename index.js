c27231f9cc975874d60c51e687ab7397dda932b1
Sitian Liu <goldensunliu@gmail.com>
2016-04-28 12:27:34 -0400

    get cookie set in response on server side
    fixes https://github.com/change/cookie-dough/issues/3

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var cookie = require('cookie');
 
 module.exports = function (req) {
+  // This is a temp hack for remembering what has been set until express has a better api for cookies
+  // see https://github.com/expressjs/express/pull/2237
+  var cookiesSet = {}
   return {
     set: function (name, value, options) {
       var cookieStr = cookie.serialize(name, value, options);
       req.res.cookie.call(req.res, name, value, options);
+      cookiesSet[name] = value
       return cookieStr;
     },
 
     get: function (name) {
-      return req.cookies[name];
+      return cookiesSet[name] || req.cookies[name];
     },
 
     remove: function (name, options) {
