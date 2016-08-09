58154b562f5a84368ca169221a392c8711e15f16
Josh Callender <jcallender@lyft.com>
2016-08-09 15:12:30 -0700

    Fix .all() and remove the requirement on req for server, we can instead use a private object and allow people to optionally pass in a req or set it themselves at the end.

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ var cookie = require('cookie');
 module.exports = function (req) {
   // This is a temp hack for remembering what has been set until express has a better api for cookies
   // see https://github.com/expressjs/express/pull/2237
-  var cookiesSet = {}
+  var cookiesSet = {};
+
   return {
     set: function (name, value, options) {
       var cookieStr = cookie.serialize(name, value, options);
-      req.res.cookie.call(req.res, name, value, options);
-      cookiesSet[name] = value
+
+      // if we don't have a request object, store everything in an object
+      // and one can return all the cookies from the set
+      if (req) {
+          req.res.cookie.call(req.res, name, value, options);
+      }
+
+      cookiesSet[name] = value;
       return cookieStr;
     },
 
@@ -20,11 +27,20 @@ module.exports = function (req) {
       var opts = options || {};
       opts.expires = new Date(0);
 
-      return !!(req.res.cookie(name, '', opts));
+      try {
+          if (req) {
+              req.res.cookie(name, '', opts)
+          }
+
+          delete cookiesSet[name];
+          return true;
+      } catch (e) {
+          return false;
+      }
     },
 
     all: function () {
-      return req.cookies;
+      return cookiesSet;
     }
   }
 };
